8ecd78976b2a27dbc94a9a8c1d1b6e39d3954cf6
Lee McCormick <lmccormick@corp.m3ts.com>
2022-04-01 18:09:28 -0500

    Finished checkboxs control

--- HelloWPF/MainWindow.xaml.cs
+++ HelloWPF/MainWindow.xaml.cs
@@ -27,7 +27,6 @@ namespace HelloWPF
             InitializeComponent();
             pnlMainGrid.MouseUp += new MouseButtonEventHandler(pnlMainGrid_MouseUp);
             txtHelloWPF.MouseDown += new MouseButtonEventHandler(helloWPF_MouseDown);
-
             // Formatting text from C#/Code-Behind ==> As you can see, formatting text through XAML is very easy, but in some cases, you might prefer or even need to do it from your C#/Code-Behind file. This is a bit more cumbersome, but here's an example on how you may do it:
             /* TextBlock tb = new TextBlock();
             tb.TextWrapping = TextWrapping.Wrap;
@@ -121,23 +120,56 @@ namespace HelloWPF
             txtStatus.Text += "Selection is " + textBox.SelectionLength + " character(s) long" + Environment.NewLine;
             txtStatus.Text += "Selected text: '" + textBox.SelectedText + "'";
         }
+
+        private void HelloButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello, Button just clicked !");
+        }
+
+        private void FormattedButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Formatted Button Just Clicked !");
+        }
+
+        private void VariousFormattedButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Various Formatted Button Just Clicked !");
+        }
+
+        private void ImageButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Image Button Just Clicked !");
+        }
+
+        private void CheckedBox_CheckedChanged(object sender, RoutedEventArgs e)
+        {
+            cbCheckAll.IsChecked = null;
+            if ((cb1.IsChecked == true) && (cb2.IsChecked == true) && (cb3.IsChecked == true) && (cb4.IsChecked == true) && (cb5.IsChecked == true) && (cb6.IsChecked == true))
+                cbCheckAll.IsChecked = true;
+            if ((cb1.IsChecked == false) && (cb2.IsChecked == false) && (cb3.IsChecked == false) && (cb4.IsChecked == false) && (cb5.IsChecked == false) && (cb6.IsChecked == false))
+                cbCheckAll.IsChecked = false;
+        }
+
+        private void CheckedAllBoxs_CheckedChanged(object sender, RoutedEventArgs e)
+        {
+            bool newVal = (cbCheckAll.IsChecked == true);
+            cb1.IsChecked = newVal;
+            cb2.IsChecked = newVal;
+            cb3.IsChecked = newVal;
+            cb4.IsChecked = newVal;
+            cb5.IsChecked = newVal;
+            cb6.IsChecked = newVal;
+        }
     }
 }
 
 /* Important Window properties
 The WPF Window class has a bunch of interesting attributes that you may set to control the look and behavior of your application window. Here's a short list of the most interesting ones:
-
 Icon - Allows you to define the icon of the window, which is usually shown in the upper left corner, to the left of the window title.
-
 ResizeMode - This controls whether and how the end-user can resize your window. The default is CanResize, which allows the user to resize the window like any other window, either by using the maximize/minimize buttons or by dragging one of the edges. CanMinimize will allow the user to minimize the window, but not to maximize it or drag it bigger or smaller. NoResize is the strictest one, where the maximize and minimize buttons are removed and the window can't be dragged bigger or smaller.
-
 ShowInTaskbar - The default is true, but if you set it to false, your window won't be represented in the Windows taskbar. Useful for non-primary windows or for applications that should minimize to the tray.
-
 SizeToContent - Decide if the Window should resize itself to automatically fit its content. The default is Manual, which means that the window doesn't automatically resize. Other options are Width, Height and WidthAndHeight, and each of them will automatically adjust the window size horizontally, vertically or both.
-
 Topmost - The default is false, but if set to true, your Window will stay on top of other windows unless minimized. Only useful for special situations.
-
 WindowStartupLocation - Controls the initial position of your window. The default is Manual, which means that the window will be initially positioned according to the Top and Left properties of your window. Other options are CenterOwner, which will position the window in the center of it's owner window, and CenterScreen, which will position the window in the center of the screen.
-
 WindowState - Controls the initial window state. It can be either Normal, Maximized or Minimized. The default is Normal, which is what you should use unless you want your window to start either maximized or minimized.
  */
